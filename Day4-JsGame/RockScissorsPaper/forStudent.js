3d51edd68aed56f5c56a02484b2476d52fb3e282
dbwlghdbwlgh <140359723+dbwlghdbwlgh@users.noreply.github.com>
2023-07-27 12:23:42 +0900

    Update forStudent.js

--- Day4-JsGame/RockScissorsPaper/forStudent.js
+++ Day4-JsGame/RockScissorsPaper/forStudent.js
@@ -111,9 +111,13 @@ const randomNumber = (n, m) => { // n부터 m까지의 랜덤한 숫자를 만�
      * n~m까지의 난수를 발생시키는 random 함수를 만드세요! (Tip. 2일차 JavaScript 기초 파일의 27페이지를 확인해보세요!)
      * return 타입은 정수 타입인 INT입니다!
      */
+    const a = Math.floor(Math.random()*(m-n+1))+n;
+    console.log(a);
+    return (a);
 };
 
 const judge = (answer, submit) => { // 결과를 "Draw", "Lose", "Win" 형태로 도출하여 String의 형태로 return하세요!
+    // let tmp = document.querySelector(".countdown");
 	/* 
      * 가위 = -1, 바위 = 0, 보자기 = 1 입니다.
      * answer에는 상대편 카드로는 -1, 0, 1 중 하나의 값이 들어옵니다.
@@ -121,4 +125,19 @@ const judge = (answer, submit) => { // 결과를 "Draw", "Lose", "Win" 형태로
      * answser와 submit의 값을 비교하여 자신이 비겼는지, 졌는지, 이겼는지 판단하는 함수를 만드세요! 
      * return 타입은 문자열인 String이며, "Draw", "Lose", "Win" 중 하나로 return되게 하면 됩니다!
      */
-};
+        if (answer === submit) {
+          return "DRAW";
+        } else if (
+            (answer === -1 && submit === 0) || 
+            (answer === 0 && submit === 1) ||  
+            (answer === 1 && submit === -1)    
+            ) {
+            return "WIN";
+        } else {
+            //   (answer === -1 && submit ===1) ||
+            //   (answer === 0 && submit === -1)||
+            //   (answer === 1 && submit === 0)
+            // tmp.innerHTML = "LOSE";
+            return "LOSE";
+        }
+      };
